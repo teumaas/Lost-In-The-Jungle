c47efa3f7c98c54e9e67abc08d57538786e7468d
Tom <tmsmits@gmail.com>
2020-06-03 16:32:31 +0200

    Added Village

---
+++ Assets/LookX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += mouseX * 3.0f;
+        transform.localEulerAngles = newRotation;
+    }
+}
