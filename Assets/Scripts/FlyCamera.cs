1c00b07def7167e178f40c871d4433cc2b083e18
Sjoerd Schepers <sjoerdschepers@hotmail.nl>
2020-06-05 10:53:31 +0200

    Added post processing and anti aliasing

--- Assets/Scripts/FlyCamera.cs
+++ Assets/Scripts/FlyCamera.cs
@@ -2,10 +2,10 @@
 using System.Collections;
  
 public class FlyCamera : MonoBehaviour {     
-    float mainSpeed = 100.0f; //regular speed
+    float mainSpeed = 25.0f; //regular speed
     float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
     float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    float camSens = 0.25f; //How sensitive it with mouse
+    float camSens = 0.1f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun= 1.0f;
      
