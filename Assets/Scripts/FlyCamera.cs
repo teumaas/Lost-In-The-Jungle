3798b8c4d4ddc3bb24a5feb1d4f989e50e1cadeb
Sjoerd Schepers <sjoerdschepers@hotmail.nl>
2020-06-05 14:56:27 +0200

    Finished post processing

--- Assets/Scripts/FlyCamera.cs
+++ Assets/Scripts/FlyCamera.cs
@@ -71,7 +71,14 @@ public class FlyCamera : MonoBehaviour {
         }
 
         if (Input.GetKeyDown(KeyCode.Tab)){
-            SceneManager.LoadScene("Village");
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            if (sceneIndex == 0){
+                SceneManager.LoadScene(1);
+            } else if (sceneIndex == 1){
+                SceneManager.LoadScene(2);
+            } else if (sceneIndex == 2){
+                SceneManager.LoadScene(0);
+            }
         }
 
         UpdateRunTime();
