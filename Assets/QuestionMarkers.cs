ade6341e53cd2ff32b04a1b1f20e217189958456
Tom Schoonbeek <34373221+TJFSchoo@users.noreply.github.com>
2020-06-09 04:39:54 +0200

    Added dynamic question data loading

--- Assets/QuestionMarkers.cs
+++ Assets/QuestionMarkers.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionMarkers : MonoBehaviour
 {
@@ -10,57 +11,86 @@ public class QuestionMarkers : MonoBehaviour
 
     public static bool gameIsPaused = false;
     public GameObject questionUI;
+    public Text questionText;
+    public Text answerOneButtonText;
+    public Text answerTwoButtonText;
+    public Text answerThreeButtonText;
 
     // Update is called once per frame
     void Update()
     {
-        // Game pause while showing question
-        // Escape key placeholder to resume
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            if(gameIsPaused){
-                if(Time.timeSinceLevelLoad >= 25){
-                    questionOneAnswered = true;
-                }
-                if(Time.timeSinceLevelLoad >= 36){
-                    questionTwoAnswered = true;
-                }
-                if(Time.timeSinceLevelLoad >= 52){
-                    questionThreeAnswered = true;
-                }
-                Resume();
-            } else {
-                Pause();
-            }
-        }
-
         // Question one trigger
         if(Time.timeSinceLevelLoad >= 26 && questionOneAnswered == false){
-            Pause();
+            Pause(1);
         }
-
         // Question two trigger
         if(Time.timeSinceLevelLoad >= 37 && questionTwoAnswered == false){
-            Pause();
+            Pause(2);
         }
-
         // Question three trigger
         if(Time.timeSinceLevelLoad >= 53 && questionThreeAnswered == false){
-            Pause();
+            Pause(3);
         }
+    }
 
-        void Resume(){
+    void Resume(){
             questionUI.SetActive(false);
             // Normal speed
             Time.timeScale = 1f;
             gameIsPaused = false;
         }
 
-        void Pause(){
-            questionUI.SetActive(true);
-            // Zero speed (Pause)
-            Time.timeScale = 0f;
-            gameIsPaused = true;
+    void Pause(int questionNumber){
+        questionUI.SetActive(true);
+        if(questionNumber == 1){
+        questionText.text = "Vraag 1";
+        answerOneButtonText.text = "Vraag 1 - Antwoord 1";
+        answerTwoButtonText.text = "Vraag 1 - Antwoord 2";
+        answerThreeButtonText.text = "Vraag 1 - Antwoord 3";
+        }else if(questionNumber == 2){
+        questionText.text = "Vraag 2";
+        answerOneButtonText.text = "Vraag 2 - Antwoord 1";
+        answerTwoButtonText.text = "Vraag 2 - Antwoord 2";
+        answerThreeButtonText.text = "Vraag 2 - Antwoord 3";
+        }else if(questionNumber == 3){
+        questionText.text = "Vraag 3";
+        answerOneButtonText.text = "Vraag 3 - Antwoord 1";
+        answerTwoButtonText.text = "Vraag 3 - Antwoord 2";
+        answerThreeButtonText.text = "Vraag 3 - Antwoord 3";
         }
+        // Zero speed (Pause)
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    void checkQuestionAnswered(){
+        if(Time.timeSinceLevelLoad >= 25){
+            questionOneAnswered = true;
+        }
+        if(Time.timeSinceLevelLoad >= 36){
+            questionTwoAnswered = true;
+        }
+        if(Time.timeSinceLevelLoad >= 52){
+            questionThreeAnswered = true;
+        }
+    }
+
+    public void clickButtonOne(){
+        Debug.Log("First answer clicked.");
+        checkQuestionAnswered();
+        Resume();
+    }
+
+    public void clickButtonTwo(){
+        Debug.Log("Second answer clicked.");
+        checkQuestionAnswered();
+        Resume();
+    }
+
+    public void clickButtonThree(){
+        Debug.Log("Third answer clicked.");
+        checkQuestionAnswered();
+        Resume();
     }
 
 }
